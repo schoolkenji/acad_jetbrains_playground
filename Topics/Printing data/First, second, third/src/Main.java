b28b51bee8842273586b70fd5afd11170536dd8a
kenjiness <f.avena.535940@umindanao.edu.ph>
2023-02-09 23:39:44 +0800

    Initial commit

---
+++ Topics/Printing data/First, second, third/src/Main.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.print("first\n" + "second\n" + "third\n" );
+    }
+}
---
+++ Topics/Printing data/Theory/src/Task.java
@@ -0,0 +1,5 @@
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+  }
+}
